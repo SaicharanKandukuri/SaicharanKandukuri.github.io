3eaba2e68359bc16f7c5e990f61c528e0ec078a9
saicharankandukuri <saicharankandukuri1x1@gmail.com>
2023-09-08 11:12:06 +0530

    some pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
+import embeds from 'astro-embed/integration';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), embeds()],
 });
